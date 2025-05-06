92993052edb5497d6f063b3c4769905bc1c71ce5
wonza-hub <wonza8585@gmail.com>
2025-05-06 19:39:32 +0900

    chore: 불필요한 import 문 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,20 @@
-import type React from "react";
-import type { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
-import { Inter } from "next/font/google";
-import "@/globals.css";
-import TopNavbar from "@/components/top-navbar";
-import localFont from "next/font/local";
+import type React from 'react';
+import { Metadata } from 'next';
+import '@/globals.css';
+import TopNavbar from '@/components/top-navbar';
+import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
-  title: "우닐의 포트폴리오",
-  description: "성장하는 프론트엔드 개발자의 포트폴리오입니다.",
+  title: '우닐의 포트폴리오',
+  description: '성장하는 프론트엔드 개발자의 포트폴리오입니다.',
 };
 
 // FONT: 프리텐다드
 const pretendard = localFont({
-  src: "../src/shared/assets/PretendardVariable.woff2",
-  display: "swap",
-  weight: "100 900",
-  variable: "--font-pretendard",
+  src: '../src/shared/assets/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '100 900',
+  variable: '--font-pretendard',
 });
 
 export default function RootLayout({
