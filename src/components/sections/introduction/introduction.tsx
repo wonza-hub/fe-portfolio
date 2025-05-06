4d5e1aafd18569e231744be8bf03359768f0db04
wonza-hub <wonza8585@gmail.com>
2025-05-06 18:35:57 +0900

    feat: 소개 섹션 추가

---
+++ src/components/sections/introduction/introduction.tsx
@@ -0,0 +1,23 @@
+export default function Introduction() {
+  return (
+    <div className="tablet:justify-center container mx-auto flex h-full flex-col items-center justify-evenly px-4 py-12">
+      <div className="mx-auto max-w-3xl space-y-6 text-center">
+        <h1 className="tablet:text-4xl desktop:text-5xl text-3xl font-bold tracking-tight after:content-['🖐️']">
+          안녕하신가요
+        </h1>
+        <p className="text-muted-foreground tablet:text-2xl text-xl">
+          프론트엔드 개발자로서
+          <br />
+          UX와 DX를 모두 고려하며
+          <br className="desktop:hidden" />
+          <span className="desktop:inline hidden">&nbsp;</span>
+          개발하는 것을 중요하게 생각합니다.
+        </p>
+      </div>
+
+      <div className="bg-muted desktop:mt-4 mt-12 flex aspect-video w-full max-w-4xl items-center justify-center rounded-lg">
+        <p className="text-muted-foreground">배경 이미지 영역</p>
+      </div>
+    </div>
+  );
+}
