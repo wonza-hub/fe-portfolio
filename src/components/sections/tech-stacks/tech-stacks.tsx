9efae603cffc0b50dfda1b491050174b7129ebe2
wonza-hub <wonza8585@gmail.com>
2025-05-11 14:40:50 +0900

    design: 기술 스택 하단 블러 처리 수정

--- src/components/sections/tech-stacks/tech-stacks.tsx
+++ src/components/sections/tech-stacks/tech-stacks.tsx
@@ -5,7 +5,7 @@ export default function TechStacks() {
   return (
     <div className="container mx-auto h-full py-24 pt-12">
       <h2 className="section-header">기술 스택</h2>
-      <div className="scrollbar-hide h-full space-y-8 overflow-auto px-6 py-10 target:px-8">
+      <div className="scrollbar-hide h-full space-y-8 overflow-auto px-6 py-10 pb-24 target:px-8">
         {techStacks.map((category) => (
           <div key={category.category}>
             <h3 className="mb-4 text-xl font-semibold">{category.category}</h3>
@@ -20,7 +20,7 @@ export default function TechStacks() {
           </div>
         ))}
         <div
-          className={`pointer-events-none absolute bottom-10 left-0 h-36 w-full bg-gradient-to-t from-white to-transparent`}
+          className={`pointer-events-none absolute bottom-0 left-0 h-72 w-full bg-gradient-to-t from-white to-transparent`}
         />
       </div>
     </div>
